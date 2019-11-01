09b6b868d9a0c0aa9b22447c3cfae93dae6ffdca
choiyunhui <55097421+choiyunhui@users.noreply.github.com>
2019-11-02 01:38:59 +0900

    초기화면구성 + 에셋추가 및 게임화면 구성

---
+++ Unity 2D Donga/Assets/Scripts/BackgroundBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject gameBackground;
+    private SpriteRenderer gameBackgroundSpriteRenderer;
+    void Start()
+    {
+        gameBackgroundSpriteRenderer = gameBackground.GetComponent<SpriteRenderer>(); // 초기화
+        StartCoroutine(FadeOut(gameBackgroundSpriteRenderer, 0.005f)); //프레임마다 0.005씩 프레임아웃 치수형값을 넣을때 f필요
+
+    }
+
+
+    IEnumerator FadeOut(SpriteRenderer spriteRenderer, float amount)
+    {
+        Color color = spriteRenderer.color;
+        while (color.a > 0.0f)
+        {
+            color.a -= amount;
+            spriteRenderer.color = color;
+            yield return new WaitForSeconds(amount);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
