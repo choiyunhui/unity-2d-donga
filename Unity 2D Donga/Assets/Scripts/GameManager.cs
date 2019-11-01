709676864c234f23e502d7e5ef13359326a7e9bc
choiyunhui <55097421+choiyunhui@users.noreply.github.com>
2019-11-02 03:29:16 +0900

    노트생성 + 판정가이드생성 + 노트판정이벤트 + 노드프리팹 생성

---
+++ Unity 2D Donga/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // GameManager를 싱글 톤 처리합니다.
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+    }
+
+    public float noteSpeed;//전체 노트는 동일한 속도로 떨어져야하기에 총 관리
+    
+    public enum judges { NONE = 0, BAD, GOOD, PERFECT, MISS};
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Unity 2D Donga/Assets/Scripts/NotesBehaviour.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int noteType; //노트 4개
+    private GameManager.judges judge;
+    private KeyCode keyCode;
+
+
+    void Start()
+    {
+        if (noteType == 1) keyCode = KeyCode.D;
+        else if (noteType == 2) keyCode = KeyCode.F;
+        else if (noteType == 3) keyCode = KeyCode.J;
+        else if (noteType == 4) keyCode = KeyCode.K;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed);//정해진 속도에 맞춰 노트가 떨어짐
+        //사용자가 노트 키를 입력한 경우
+        if (Input.GetKey(keyCode))
+        {
+            //해당 노트에 대한 판정을 진행합니다.
+            Debug.Log(judge);
+            //노트가 판정 선에 닿기 시작한 이후로는 해당 노트를 제거합니다.
+            if (judge != GameManager.judges.NONE) Destroy(gameObject);
+        }
+
+    }
+    //각 노트의 현재 위치에 대하여 판정을 수행합니다.
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Bad_Line") //안에 문자가 정확히 들어가야한다.
+        {
+            judge = GameManager.judges.BAD;
+        }
+        else if(other.gameObject.tag == "Good_Line")
+        {
+            judge = GameManager.judges.GOOD;
+        }
+        else if (other.gameObject.tag == "Perfect_Line")
+        {
+            judge = GameManager.judges.PERFECT;
+        }
+        else if (other.gameObject.tag == "Miss_Line")
+        {
+            judge = GameManager.judges.MISS;
+            Destroy(gameObject);
+        }
+       
+    }
+}
